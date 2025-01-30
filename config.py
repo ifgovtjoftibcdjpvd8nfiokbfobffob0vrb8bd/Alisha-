2da3bd9f50ac5140e2b472ce4b5cae79933b440d
ifgovtjoftibcdjpvd8nfiokbfobffob0vrb8bd <sonkarp463@gmail.com>
2025-01-31 00:53:02 +0530

    Update config.py

--- config.py
+++ config.py
@@ -4,14 +4,14 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-API_ID = 6435225
+API_ID = 26341381
 # -------------------------------------------------------------
-API_HASH = "4e984ea35f854762dcde906dce426c2d"
+API_HASH = "2762d6b85975700ffddae249e13911cf"
 # --------------------------------------------------------------
-BOT_TOKEN = getenv("BOT_TOKEN", "7833972330:AAErp7yd_GYU9UyvE84tyfvFlb0KctRe9do")
+BOT_TOKEN = getenv("BOT_TOKEN", "8044536901:AAEdh9Pj3iCvEF_r5aKASuXoY9d4VSaQ-to")
 STRING1 = getenv("STRING_SESSION", None)
 MONGO_URL = getenv("MONGO_URL", "mongodb+srv://theriyamusic94:f67KlgTyzr3TTutn@cluster0.lym5x.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
-OWNER_ID = int(getenv("OWNER_ID", 7668520999))
+OWNER_ID = int(getenv("OWNER_ID", 8009369344))
 SUPPORT_GRP = "RIYA_chat_SUPPORT"
 UPDATE_CHNL = "riya_NETWORK"
 OWNER_USERNAME = "ll_hunter_lll"
